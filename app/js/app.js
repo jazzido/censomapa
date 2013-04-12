15064b1d44b004b20c154df16003f53f268811e4
cbertelegni <cristianbertelegni@gmail.com>
2013-04-12 17:23:02 -0300

    tooltip

--- app/js/app.js
+++ app/js/app.js
@@ -142,6 +142,7 @@ $(function() {
     // cargar geometrias
     $.get('data/ea.json', function(topojson) {
         mapa.drawPaths(topojson, 'article#svg');
+        var tootip = new moverObjMouseOver($("path"), $("#tooltip"), $("svg"));
 
         // popular distrito_info_dict (convenience)
         topojson.objects.departamentos.geometries.forEach(function(g) {
